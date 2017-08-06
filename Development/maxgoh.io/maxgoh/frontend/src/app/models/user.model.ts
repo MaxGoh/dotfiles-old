bf3b41fedd844648af38662b671bf7c197e26573
Max Goh <maxgohjh@gmail.com>
2017-08-06 13:52:07 +0800

    remove

--- Development/maxgoh.io/maxgoh/frontend/src/app/models/user.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Draft {
-  id: number;
-  title: string;
-}
-
-
--- Development/maxgoh.io/maxgoh/frontend/src/app/pipes/truncate.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({'name': 'truncate'})
-export class TruncatePipe implements PipeTransform {
-  transform(text: string, count: number): string {
-    return text.slice(0, count);
-  };
-}
